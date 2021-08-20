2583ff6eb68eea60b9bcf0b6f3511377fb483420
Kunal <kdshah6593@gmail.com>
2021-08-20 13:04:03 -0400

    completed solution, all tests passing

---
+++ index.js
@@ -0,0 +1,28 @@
+// string is assumed to have only unique characters
+
+function findAllPermutations(string) {
+    if (string.length < 2) return [string]
+    
+    let permutationsArr = [];
+
+    for (let i = 0; i < string.length; i++) {
+        let char = string[i]
+        let remainingString = string.slice(0,i) + string.slice(i+1, string.length)
+        console.log(`Char: ${char}`)
+        console.log(`Remaining: ${remainingString}`)
+        
+
+        for (let permutation of findAllPermutations(remainingString)) {
+            console.log(`Permutation: ${permutation}`)
+            permutationsArr.push(char + permutation)
+            console.log(permutationsArr)
+            console.log(" ")
+        }
+    }
+
+    return permutationsArr
+}
+
+
+// findAllPermutations("abc")
+// console.log(findAllPermutations("abcd"))
