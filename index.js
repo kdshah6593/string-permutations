60d67e340452af184ff3e2637bd8b39ea2956a2d
Kunal <kdshah6593@gmail.com>
2021-08-20 13:09:48 -0400

    completed solution, all tests passing

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ function findAllPermutations(string) {
 
     for (let i = 0; i < string.length; i++) {
         let char = string[i]
+
+        // if (string.indexOf(char) != i) {continue} | this line is if the string did not have only unique characters
+
         let remainingString = string.slice(0,i) + string.slice(i+1, string.length)
         console.log(`Char: ${char}`)
         console.log(`Remaining: ${remainingString}`)
